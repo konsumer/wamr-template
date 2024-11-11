caab008c6f9243a9d3bcd3adbd5765613fbe1910
David Konsumer <83857+konsumer@users.noreply.github.com>
2024-11-11 12:46:31 -0800

    Update main.c

--- host/src/main.c
+++ host/src/main.c
@@ -1,7 +1,12 @@
 #include "host.h"
 
 int main(int argc, char *argv[]) {
-  wasm_host_load("build/cart/main.wasm");
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <CART_FILE>\n", argv[0]);
+    return 1;
+  }
+  
+  wasm_host_load(argv[1]);
 
   if (cart_update != NULL) {
     while (1) {
